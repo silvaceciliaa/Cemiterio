9aaa680d3e1cb7615dcb2987fd18c4ecd79517f1
Cecília <103593379+silvaceciliaa@users.noreply.github.com>
2022-08-27 13:31:36 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using Cemiterio;
+
+Registro[] registros = new Registro[10];
+bool loop = true; 
+
+for (int i = 0; i < registros.Length; i++)
+{
+    registros[i] = new Registro();
+}
+
+while (loop)
+{
+    Console.WriteLine("Bem-vindo ao cemitério Morada da Paz\n Selecione uma das opções do menu:\n 1 - Enterro\n 2 - Conferir disponibilidade de cova\n 3 - Sair");
+    string opcaoMenu = Console.ReadLine();
+
+    if (opcaoMenu == "1")
+    {
+        bool achouCova = false;
+        for (int i = 0; i < registros.Length; i++)
+        {
+            if (registros[i].Nome == null)
+            {
+                Console.Clear();
+                achouCova = true;
+                registros[i].Enterro(i);
+                break;
+            }
+        }
+        if (achouCova == false)
+        {
+            Console.Clear();
+            Console.WriteLine("Não existe espaço disponível\n");
+        }
+    }
+    if (opcaoMenu == "2")
+    {
+        for (int i = 0; i < registros.Length; i++)
+        {
+            Console.Clear();
+            Console.WriteLine($"Nome: {registros[i].Nome} - Ano da morte {registros[i].AnoMorte} - Cova {registros[i].Cova}");
+        }
+    }
+    if (opcaoMenu == "3")
+    {
+        Console.WriteLine("Obrigado por se enterrar aqui!");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Selecione uma das opções do menu");
+    }
+}
---
+++ Registro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Cemiterio
+{
+    public class Registro
+    {
+        public string Nome { get; set; }
+
+        public int AnoMorte { get; set; }
+
+        public int Cova { get; set; }
+
+        public void Enterro(int cova)
+        {
+            Console.WriteLine("Digite o nome do morto");
+            Nome = Console.ReadLine();
+            Console.WriteLine("Digite o ano de sua morte");
+            AnoMorte = Convert.ToInt32(Console.ReadLine());
+            Cova = cova + 1;
+            Console.WriteLine($"Morto enterrado com sucesso na cova {Cova}");
+        }
+    }
+}
+
